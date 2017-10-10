d714f74b4b626fe2115dbdf9f1c1d2f2c8acf3db
Kimaya Desai <kimaya.desai@gmail.com>
2017-10-10 02:43:27 -0700

    Update ViewController.swift

--- Camera-Object-Detection/ViewController.swift
+++ Camera-Object-Detection/ViewController.swift
@@ -28,16 +28,13 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
         previewLayer.frame = view.frame
         
         let dataOutput = AVCaptureVideoDataOutput()
-        dataOutput.setSampleBufferDelegate(self, queue:
-            DispatchQueue(label: "videoQueue"))
+        dataOutput.setSampleBufferDelegate(self, queue:DispatchQueue(label: "videoQueue"))
         captureSession.addOutput(dataOutput)
-   
     }
     
     func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection:
         AVCaptureConnection) {
-    //    print("Camera was able to capture the frame", Date())
-        
+
         guard let pixelBuffer : CVPixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else
         { return }
         
@@ -46,25 +43,20 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
         
         let request = VNCoreMLRequest(model: model)
         { (finishedReq, err) in
-           // print(finishedReq.results)
-            
-            guard let results = finishedReq.results as?
-                [VNClassificationObservation] else {return}
-          
-            guard let firstObservation = results.first else
-            {return}
-            
-            print(firstObservation.identifier,firstObservation.confidence)
-            
-            
-        }
-
-        try? VNImageRequestHandler(cvPixelBuffer: pixelBuffer, options: [ : ]).perform([request])
         
+            
+        guard let results = finishedReq.results as?
+        [VNClassificationObservation] else {return}
+          
+        guard let firstObservation = results.first else
+        {return}
+            
+        print(firstObservation.identifier,firstObservation.confidence)
+            
+            
+     }
+        try? VNImageRequestHandler(cvPixelBuffer: pixelBuffer, options: [ : ]).perform([request])  
     }
-    
-    @IBOutlet weak var x: UILabel!
 
-    
 }
 
